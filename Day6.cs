1f1ca4fe047d59c77f82517ed079964570214908
Kael Larkin <kael.larkin@purple.telstra.com>
2022-12-06 16:49:37 +1000

    Day 6 and making execution automatic

---
+++ Day6.cs
@@ -0,0 +1,30 @@
+﻿public class Day6 : BaseDay
+{
+    public override void Execute()
+    {
+        var text = File.ReadAllText(InputFile);
+        var startOfPacketMarker = GetMarkerStart(text, 4);
+        var startOfMessageMarker = GetMarkerStart(text, 14);
+        WriteOutput(startOfPacketMarker, startOfMessageMarker);
+    }
+
+    private static int GetMarkerStart(string text, int markerSize)
+    {
+        var initialBuffer = text.Take(markerSize);
+        var queue = new Queue<char>(initialBuffer);
+        int startOfMarker = markerSize;
+        foreach(var c in text.Skip(markerSize))
+        {
+            if (queue.Distinct().Count() == markerSize)
+            {
+                break;
+            }
+
+            queue.Dequeue();
+            queue.Enqueue(c);
+            startOfMarker++;
+        }
+
+        return startOfMarker;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,10 @@
-﻿new Day1().Execute();
-new Day2().Execute();
-new Day3().Execute();
-new Day4().Execute();
-new Day5().Execute();
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+
+var days = typeof(Day1).Assembly.GetTypes()
+    .Where(x => !x.IsAbstract && x.Name.Contains("Day"))
+    .Select(x => new { name = x.Name, type = Activator.CreateInstance(x) as BaseDay })
+    .OrderBy(x => x.name);
+
+foreach (var day in days)
+    day.type!.Execute();
