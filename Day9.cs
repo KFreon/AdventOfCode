8330fe2306af1703f08587887f133658aed356f4
Kael Larkin <kael.larkin@purple.telstra.com>
2022-12-09 20:45:10 +1000

    Finish Day9

--- Day9.cs
+++ Day9.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-public class Day9 : BaseDay
+﻿public class Day9 : BaseDay
 {
     record Position(int X, int Y);
 
@@ -16,8 +14,64 @@ public class Day9 : BaseDay
     {
         var lines = File.ReadAllLines(InputFile);
         var numberOfDistinctLocations = CountNumberOfDistinctLocationsOccupiedByTail(lines);
+        var numberOfDistinctLocations_chain = CountDistinctLocationsForTailInChain(lines);
 
-        WriteOutput(numberOfDistinctLocations);
+        WriteOutput(numberOfDistinctLocations, numberOfDistinctLocations_chain);
+    }
+
+    private static int CountDistinctLocationsForTailInChain(string[] lines)
+    {
+        var knots = Enumerable.Range(0, 10).Select(x => new Position(0,0)).ToArray();
+        var tailMoves = new List<Position> { new Position(0, 0) };
+
+        foreach (var line in lines)
+        {
+            var direction = (Direction)line[0];
+            var distance = int.Parse(line[2..]);
+
+            for (var step = 0; step < distance; step++)
+            {
+                knots[0] = MoveHead(direction, knots[0]);
+                var currentTailPosition = knots.Last() with { };
+
+                for (var knotIndex = 1; knotIndex < knots.Length; knotIndex++)
+                {
+                    var previousKnot = knots[knotIndex - 1] with { };
+                    var thisKnotPosition = knots[knotIndex] with { };
+                    knots[knotIndex] = GetNewPosition(previousKnot, thisKnotPosition);
+                }
+
+                var newTailPosition = knots.Last();
+                if (currentTailPosition != newTailPosition)
+                {
+                    tailMoves.Add(newTailPosition with { });
+                }
+            }
+        }
+
+        // example = 36
+        return tailMoves.Distinct().Count();
+    }
+
+    private static Position MoveHead(Direction direction, Position head)
+    {
+        switch (direction)
+        {
+            case Direction.Left:
+                head = head with { X = head.X - 1 };
+                break;
+            case Direction.Right:
+                head = head with { X = head.X + 1 };
+                break;
+            case Direction.Up:
+                head = head with { Y = head.Y + 1 };
+                break;
+            case Direction.Down:
+                head = head with { Y = head.Y - 1 };
+                break;
+        }
+
+        return head;
     }
 
     private static int CountNumberOfDistinctLocationsOccupiedByTail(string[] lines)
@@ -33,21 +87,7 @@ public class Day9 : BaseDay
             for (int i = 0; i < distance; i++)
             {
                 var previous = headPosition;
-                switch (direction)
-                {
-                    case Direction.Left:
-                        headPosition = headPosition with { X = headPosition.X - 1 };
-                        break;
-                    case Direction.Right:
-                        headPosition = headPosition with { X = headPosition.X + 1 };
-                        break;
-                    case Direction.Up:
-                        headPosition = headPosition with { Y = headPosition.Y + 1 };
-                        break;
-                    case Direction.Down:
-                        headPosition = headPosition with { Y = headPosition.Y - 1 };
-                        break;
-                }
+                headPosition = MoveHead(direction, headPosition);
 
                 var xDiff = headPosition.X - previousHeadPosition.X;
                 var yDiff = headPosition.Y - previousHeadPosition.Y;
@@ -55,7 +95,7 @@ public class Day9 : BaseDay
                 var absX = Math.Abs(xDiff);
                 var absY = Math.Abs(yDiff);
 
-                if (absX > 1 || absY> 1) 
+                if (absX > 1 || absY > 1)
                 {
                     previousHeadPosition = previous;
                     tailMoves.Add(previousHeadPosition);
@@ -64,5 +104,27 @@ public class Day9 : BaseDay
         }
 
         return tailMoves.Distinct().Count();
+    }
+
+    private static Position GetNewPosition(Position previousKnotCurrentPosition, Position thisOne)
+    {
+        var xDiff = previousKnotCurrentPosition.X - thisOne.X;
+        var yDiff = previousKnotCurrentPosition.Y - thisOne.Y;
+
+        var absX = Math.Abs(xDiff);
+        var absY = Math.Abs(yDiff);
+
+        if (absX > 1 || absY > 1)
+        {
+            // Needs to move towards it
+
+            //xxxxxx
+            //xxxftx
+
+            //xxxxxt
+            //xxxfxx
+            return thisOne with { X = thisOne.X + Math.Clamp(xDiff, -1, 1), Y = thisOne.Y + Math.Clamp(yDiff, -1, 1) };
+        }
+        return thisOne with { };
     }
 }
