6d04c9dc7882bcb36bdc1684c1cd3f3b1fee544f
Kael Larkin <kael.larkin@purple.telstra.com>
2022-12-09 11:44:42 +1000

    Playing with better indexes for Day8

--- Day8.cs
+++ Day8.cs
@@ -24,12 +24,89 @@ public class Day8 : BaseDay
                 var currentTree = lines[row][column].ToString();
                 if (trees[row] == default)
                 {
-                    trees[row] = new Tree[width]; 
+                    trees[row] = new Tree[width];
                 }
                 trees[row][column] = new Tree(int.Parse(currentTree), isEdge);
             }
         }
 
+        var numberOfTreesVisible_Indexes = GetUsingIndexes(height, width, trees);
+        var numberOfTreesVisible = GetUsingDumbMethod(height, width, trees);
+        //var bestSpot_Indexes = GetScoreUsingIndexes(height, width, trees);
+        var bestSpot = GetScoreUsingDumbMethod(height, width, trees);
+
+        WriteOutput(numberOfTreesVisible, bestSpot);
+        WriteOutput(numberOfTreesVisible_Indexes);
+    }
+
+    private static int GetScoreUsingDumbMethod(int height, int width, Tree[][] trees)
+    {
+        for (var row = 1; row < height - 1; row++)
+        {
+            for (var column = 1; column < width - 1; column++)
+            {
+                var current = trees[row][column];
+                var currentHeight = current.Height;
+
+                // top
+                var top = 0;
+                for (top = row - 1; top > 0; top--)
+                {
+                    if (trees[top][column].Height >= currentHeight)
+                    {
+                        break;
+                    }
+                }
+
+                var topScore = row - top;
+
+                // bottom
+                var bottom = 0;
+                for (bottom = row + 1; bottom < height - 1; bottom++)
+                {
+                    if (trees[bottom][column].Height >= currentHeight)
+                    {
+                        break;
+                    }
+                }
+
+                var bottomScore = bottom - row;
+
+                // left
+                var left = 0;
+                for (left = column - 1; left > 0; left--)
+                {
+                    if (trees[row][left].Height >= currentHeight)
+                    {
+                        break;
+                    }
+                }
+
+                var leftScore = column - left;
+
+                // right
+                var right = 0;
+                for (right = column + 1; right < height - 1; right++)
+                {
+                    if (trees[row][right].Height >= currentHeight)
+                    {
+                        break;
+                    }
+                }
+
+                var rightScore = right - column;
+
+                var totalScore = topScore * bottomScore * leftScore * rightScore;
+                trees[row][column] = current with { Score = totalScore };
+            }
+        }
+
+        var bestSpot = trees.SelectMany(x => x.Select(t => t.Score)).Max();
+        return bestSpot;
+    }
+
+    private static int GetUsingDumbMethod(int height, int width, Tree[][] trees)
+    {
         var maxHeightsTop = trees[0].Clone() as Tree[];
         var maxHeightsBottom = trees.Last().Clone() as Tree[];
 
@@ -91,70 +168,31 @@ public class Day8 : BaseDay
 
         // Overlapping corners
         var numberOfTreesVisible = trees.SelectMany(x => x.Select(t => t.IsVisible)).Where(x => x == true).Count();
+        return numberOfTreesVisible;
+    }
 
-
-        for(var row = 1; row < height - 1; row++)
+    private static int GetUsingIndexes(int height, int width, Tree[][] trees)
+    {
+        var treesWithVisibility = trees.Select((treeRow, row) => treeRow.Select((tree, column) =>
         {
-            for(var column = 1; column < width - 1; column++)
+            var isEdge = row == 0 || row == height - 1
+                    || column == 0 || column == width - 1;
+
+            var isVisible = isEdge;
+            if (!isVisible)
             {
-                var current= trees[row][column];
-                var currentHeight = current.Height;
-
-                // top
-                var top = 0;
-                for(top = row - 1; top > 0; top--)
-                {
-                    if (trees[top][column].Height >= currentHeight)
-                    {
-                        break;
-                    }
-                }
-
-                var topScore = row - top;
-
-                // bottom
-                var bottom = 0;
-                for(bottom = row + 1; bottom < height - 1; bottom++)
-                {
-                    if (trees[bottom][column].Height >= currentHeight)
-                    {
-                        break;
-                    }
-                }
-
-                var bottomScore = bottom - row;
-
-                // left
-                var left = 0;
-                for(left = column - 1; left > 0; left--)
-                {
-                    if (trees[row][left].Height >= currentHeight)
-                    {
-                        break;
-                    }
-                }
-
-                var leftScore = column - left;
-
-                // right
-                var right = 0;
-                for(right = column + 1; right < height - 1; right++)
-                {
-                    if (trees[row][right].Height >= currentHeight)
-                    {
-                        break;
-                    }
-                }
-
-                var rightScore = right - column;
-
-                var totalScore = topScore * bottomScore * leftScore * rightScore;
-                trees[row][column] = current with { Score = totalScore };
+                var visibleFromLeft = treeRow[..column].All(t => t.Height < tree.Height);
+                var visibleFromRight = treeRow[(column + 1)..].All(t => t.Height < tree.Height);
+                var visibleFromTop = trees[..row].Select(x => x[column]).All(t => t.Height < tree.Height);
+                var visibleFromBottom = trees[(row + 1)..].Select(x => x[column]).All(t => t.Height < tree.Height);
+                isVisible = visibleFromLeft || visibleFromRight || visibleFromTop || visibleFromBottom;
             }
-        }
+            if (isVisible) return tree with { IsVisible = true };
+            return tree;
+        }).ToArray()
+                ).ToArray();
 
-        var bestSpot = trees.SelectMany(x => x.Select(t => t.Score)).Max();
-
-        WriteOutput(numberOfTreesVisible, bestSpot);
+        var howManyVisible = treesWithVisibility.SelectMany(x => x.Select(t => t.IsVisible)).Count(x => x == true);
+        return howManyVisible;
     }
 }
