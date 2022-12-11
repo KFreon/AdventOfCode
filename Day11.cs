be875c59671685d5c832198775d2f3f0e4551b6a
Kael Larkin <kael.larkin@purple.telstra.com>
2022-12-11 21:02:32 +1000

    Stuck on day 11

---
+++ Day11.cs
@@ -0,0 +1,111 @@
+﻿public class Day11 : BaseDay
+{
+    enum OperationType
+    {
+        Add = '+',
+        Multiply = '*'
+    }
+
+    record Operation(OperationType OperationType, uint Amount);
+
+    class Monkey
+    {
+        public int Index { get; set; }
+        public List<ulong> Items { get; set; }
+        public Operation Operation { get; set; }
+        public uint TestDivideAmount { get; set; }
+        public int TrueTarget { get; set; }
+        public int FalseTarget { get; set; }
+        public ulong ItemInspectionCount { get; set; }
+
+        public Monkey(string[] lines)
+        {
+            Index = int.Parse(lines[0]["Monkey ".Length..^1]);
+
+            Items = lines[1]["  Starting items:".Length..]
+                .Split(',')
+                .Select(ulong.Parse)
+                .ToList();
+
+            var operationChar = lines[2]["  Operation: new = old ".Length];
+            var operation = (OperationType)operationChar;
+            var amount = lines[2][("  Operation: new = old ".Length + 1)..];
+            Operation = new Operation(operation, amount.Contains("old") ? 0 : uint.Parse(amount));
+
+            TestDivideAmount = uint.Parse(lines[3]["  Test: divisible by ".Length..]);
+
+            TrueTarget = int.Parse(lines[4]["    If true: throw to monkey ".Length..]);
+            FalseTarget = int.Parse(lines[5]["    If false: throw to monkey ".Length..]);
+        }
+
+        public bool Test(ulong item) => item % TestDivideAmount == 0L;
+
+        public ulong Inspect_AndGetNewWorryLevelForItem(ulong item)
+        {
+            ItemInspectionCount++;
+            return Operation.OperationType switch
+            {
+                OperationType.Add => item + (Operation.Amount == 0 ? item : Operation.Amount),
+                OperationType.Multiply => item * (Operation.Amount == 0 ? item : Operation.Amount),
+                _ => throw new NotImplementedException()
+            };
+        }
+
+        public void TakeTurn(Monkey[] allMonkeys, bool areWeWorried, ulong round)
+        {
+            if (!Items.Any())
+            {
+                return;
+            }
+
+            foreach (var item in Items)
+            {
+                var newWorryLevelForItem = Inspect_AndGetNewWorryLevelForItem(item);
+                ulong relief = areWeWorried 
+                    ? newWorryLevelForItem 
+                    : (ulong)Math.Round(newWorryLevelForItem / 3d, MidpointRounding.ToZero);
+
+                var thing = relief / round;
+                relief = (ulong)Math.Round(thing);
+
+                var panicThrow = Test(relief);
+                allMonkeys[panicThrow ? TrueTarget : FalseTarget].Items.Add(relief);
+            }
+            Items.Clear();
+        }
+    }
+
+    public override void Execute()
+    {
+        //var monkeyBusiness = GetMonkeyBusiness(lines.Value, false, 20);
+        var fear = GetMonkeyBusiness(lines.Value, true, 10_000);
+
+        WriteOutput(fear);
+    }
+
+    private static ulong GetMonkeyBusiness(string[] lines, bool areWeWorried, int numberOfRounds)
+    {
+        var monkeys = lines.Chunk(7).Select(x => new Monkey(x)).ToArray();
+
+        var roundCountsOfInterest = new[] { 1, 20, 1000, 2000 };
+
+        var things = new List<(int Index, ulong ItemInspectionCount)[]>();
+
+        for (int round = 0; round < numberOfRounds; round++)
+        {
+            if (roundCountsOfInterest.Contains(round))
+            {
+                things.Add(monkeys.Select((x, i) => (Index: i, x.ItemInspectionCount)).ToArray());
+            }
+
+            for (int monkey = 0; monkey < monkeys.Length; monkey++)
+            {
+                monkeys[monkey].TakeTurn(monkeys, areWeWorried, (ulong)round);
+            }
+        }
+
+        var top2 = monkeys.OrderByDescending(x => x.ItemInspectionCount).Take(2);
+        var monkeyBusiness = top2.Aggregate(1UL, (agg, monkey) => agg * monkey.ItemInspectionCount);
+        return monkeyBusiness;
+    }
+}
